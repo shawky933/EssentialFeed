5cec2086fb5419283d32fa95d84378f57eb89b79
Shawky Elhanak <shawky@elhanak.com>
2024-11-11 14:58:07 +0100

    Remove references of "Items" in favor of "Images" which is a domain term used by domain experts in the specs

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromUseCaseTests.swift
@@ -133,8 +133,8 @@ class LoadFeedFromUseCaseTests: XCTestCase {
         description: String? = nil,
         location: String? = nil,
         imageURL: URL
-    ) -> (model: FeedItem, json: [String: Any]) {
-        let item = FeedItem(id: id, description: description, location: location, imageURL: imageURL)
+    ) -> (model: FeedImage, json: [String: Any]) {
+        let item = FeedImage(id: id, description: description, location: location, url: imageURL)
         
         let json = [
             "id": id.uuidString,
