660bc4d13b03984bd51971a036f802a5f52cad3e
Shawky Elhanak <shawky@elhanak.com>
2024-11-28 11:22:29 +0100

    Show loading indicator on pull to refresh

--- EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -82,6 +82,15 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
     }
 
+    func test_pullToRefresh_showsLoadingIndicator() {
+        let (sut, _) = makeSut()
+
+        sut.simulateAppearance()
+        sut.refreshControl?.simulatePullToRefresh()
+        
+        XCTAssertTrue(sut.refreshControl?.isRefreshing ?? true)
+    }
+
     // MARK: - Helpers
 
     private func makeSut(
