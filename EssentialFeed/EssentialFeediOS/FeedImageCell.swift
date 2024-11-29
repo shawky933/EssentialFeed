6373dc9ab1fbf2282ed8c4cbd34d415ca057de21
Shawky Elhanak <shawky@elhanak.com>
2024-11-29 13:13:27 +0100

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeed
+//
+//  Created by Shawky Elhanak on 29.11.24.
+//
+
+import UIKit
+
+public class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
