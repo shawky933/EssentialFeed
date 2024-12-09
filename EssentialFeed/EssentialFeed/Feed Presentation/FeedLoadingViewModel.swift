ce99ce94adc086cf2741f4beae1330b8c05281cc
Shawky Elhanak <shawky@elhanak.com>
2024-12-09 15:14:41 +0100

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Shawky Elhanak on 09.12.24.
+//
+
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
