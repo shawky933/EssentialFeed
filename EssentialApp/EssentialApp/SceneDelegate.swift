8066b8a49cc739e068f4b6ae5098216e54890f18
Shawky Elhanak <shawky@elhanak.com>
2024-12-19 11:30:39 +0100

    Displays remote feed on launch when customer has connectivity

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import CoreData
 import EssentialFeed
 import EssentialFeediOS
 
@@ -20,6 +19,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         options connectionOptions: UIScene.ConnectionOptions
     ) {
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        let remoteURL = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+
+        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+
+        window?.rootViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: remoteFeedLoader,
+            imageLoader: remoteImageLoader
+        )
     }
 }
 
