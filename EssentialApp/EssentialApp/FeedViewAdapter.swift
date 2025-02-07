9abe7ba3b31a196fead8d50feb5be9368aad8119
Shawky Elhanak <shawky@elhanak.com>
2025-02-07 21:10:59 +0100

    Move UIImage creation to a `tryMake` extension

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -35,13 +35,7 @@ final class FeedViewAdapter: ResourceView {
                 resourceView: WeakRefVirtualProxy(view),
                 loadingView: WeakRefVirtualProxy(view),
                 errorView: WeakRefVirtualProxy(view),
-                mapper: { data in
-                    guard let image = UIImage(data: data) else {
-                        throw InvalidImageData()
-                    }
-                    
-                    return image
-                }
+                mapper: UIImage.tryMake
             )
 
             return view
@@ -49,4 +43,13 @@ final class FeedViewAdapter: ResourceView {
     }
 }
 
-struct InvalidImageData: Error {}
+extension UIImage {
+    struct InvalidImageData: Error {}
+
+    static func tryMake(data: Data) throws -> UIImage {
+        guard let image = UIImage(data: data) else {
+            throw InvalidImageData()
+        }
+        return image
+    }
+}
