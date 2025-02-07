04509c44f560f6fd2123bbf050610a1535262b2e
Shawky Elhanak <shawky@elhanak.com>
2025-02-07 21:09:36 +0100

    Add type aliases to shorten type definitions with generics

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -13,6 +13,11 @@ final class FeedViewAdapter: ResourceView {
     private weak var controller: FeedViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
 
+    private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<
+        Data,
+        WeakRefVirtualProxy<FeedImageCellController>
+    >
+
     init(controller: FeedViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
         self.controller = controller
         self.imageLoader = imageLoader
@@ -20,9 +25,7 @@ final class FeedViewAdapter: ResourceView {
 
     func display(_ viewModel: FeedViewModel) {
         controller?.display(viewModel.feed.map { model in
-            let adapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>(
-                loader: { [imageLoader] in imageLoader(model.url) }
-            )
+            let adapter = ImageDataPresentationAdapter(loader: { [imageLoader] in imageLoader(model.url) })
 
             let view = FeedImageCellController(
                 viewModel: FeedImagePresenter.map(model), delegate: adapter
