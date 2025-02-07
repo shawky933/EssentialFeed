51a6d4388a2cc910682fb1381e177178897975c1
Shawky Elhanak <shawky@elhanak.com>
2025-02-07 13:36:34 +0100

    Replace FeedImagePresenter with LoadResourcePresenter

--- EssentialApp/EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/EssentialApp/FeedViewAdapter.swift
@@ -20,17 +20,30 @@ final class FeedViewAdapter: ResourceView {
 
     func display(_ viewModel: FeedViewModel) {
         controller?.display(viewModel.feed.map { model in
-            let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(
-                model: model,
-                imageLoader: imageLoader
+            let adapter = LoadResourcePresentationAdapter<Data, WeakRefVirtualProxy<FeedImageCellController>>(
+                loader: { [imageLoader] in imageLoader(model.url) }
             )
-            let view = FeedImageCellController(delegate: adapter)
 
-            adapter.presenter = FeedImagePresenter(
-                view: WeakRefVirtualProxy(view),
-                imageTransformer: UIImage.init)
+            let view = FeedImageCellController(
+                viewModel: FeedImagePresenter<FeedImageCellController, UIImage>.map(model), delegate: adapter
+            )
+
+            adapter.presenter = LoadResourcePresenter(
+                resourceView: WeakRefVirtualProxy(view),
+                loadingView: WeakRefVirtualProxy(view),
+                errorView: WeakRefVirtualProxy(view),
+                mapper: { data in
+                    guard let image = UIImage(data: data) else {
+                        throw InvalidImageData()
+                    }
+                    
+                    return image
+                }
+            )
 
             return view
         })
     }
 }
+
+struct InvalidImageData: Error {}
