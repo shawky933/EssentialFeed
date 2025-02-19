01efe02f1e7dd3c8f12c75d8df3da0f32a50f7f5
Shawky Elhanak <shawky@elhanak.com>
2025-02-19 16:50:58 +0100

    Add "Load more" message label

--- EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/FeedSnapshotTests.swift
@@ -46,6 +46,18 @@ class FeedSnapshotTests: XCTestCase {
         assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_LOAD_MORE_INDICATOR_dark")
     }
 
+    func test_feedWithLoadMoreError() {
+        let sut = makeSUT()
+
+        sut.display(feedWithLoadMoreError())
+
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .light)), named: "FEED_WITH_LOAD_MORE_ERROR_light")
+        assert(snapshot: sut.snapshot(for: .iPhone8(style: .dark)), named: "FEED_WITH_LOAD_MORE_ERROR_dark")
+        assert(
+            snapshot: sut.snapshot(for: .iPhone8(style: .light, contentSize: .extraExtraExtraLarge)),
+            named: "FEED_WITH_LOAD_MORE_ERROR_light_extraExtraExtraLarge"
+        )
+    }
 
     // MARK: - Helpers
 
@@ -90,13 +102,26 @@ class FeedSnapshotTests: XCTestCase {
     }
 
     private func feedWithLoadMoreIndicator() -> [CellController] {
+        let loadMore = LoadMoreCellController()
+        loadMore.display(ResourceLoadingViewModel(isLoading: true))
+        return feedWith(loadMore: loadMore)
+    }
+
+    private func feedWithLoadMoreError() -> [CellController] {
+        let loadMore = LoadMoreCellController()
+        loadMore.display(ResourceErrorViewModel(message: "This is a multi-line\nerror message"))
+        return feedWith(loadMore: loadMore)
+    }
+
+    private func feedWith(loadMore: LoadMoreCellController) -> [CellController] {
         let stub = feedWithContent().last!
         let cellController = FeedImageCellController(viewModel: stub.viewModel, delegate: stub, selection: {})
         stub.controller = cellController
 
-        let loadMore = LoadMoreCellController()
-        loadMore.display(ResourceLoadingViewModel(isLoading: true))
-        return [CellController(id: UUID(), cellController), CellController(id: UUID(), loadMore)]
+        return [
+            CellController(id: UUID(), cellController),
+            CellController(id: UUID(), loadMore),
+        ]
     }
 }
 
