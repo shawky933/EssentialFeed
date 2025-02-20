1dcacdef2de629d78cd44ab2b2c27eba04648400
Shawky Elhanak <shawky@elhanak.com>
2025-02-20 13:29:29 +0100

    Load more items on will display LoadMoreCell

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -67,11 +67,24 @@ extension ListViewController {
         }
         refreshControl = fake
     }
+
+    func numberOfRows(in section: Int) -> Int {
+        tableView.numberOfSections > section ? tableView.numberOfRows(inSection: section) : 0
+    }
+
+    func cell(row: Int, section: Int) -> UITableViewCell? {
+        guard numberOfRows(in: section) > row else {
+            return nil
+        }
+        let ds = tableView.dataSource
+        let index = IndexPath(row: row, section: section)
+        return ds?.tableView(tableView, cellForRowAt: index)
+    }
 }
 
 extension ListViewController {
     func numberOfRenderedComments() -> Int {
-        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
+        numberOfRows(in: commentsSection)
     }
 
     func commentMessage(at row: Int) -> String? {
@@ -87,12 +100,7 @@ extension ListViewController {
     }
 
     private func commentView(at row: Int) -> ImageCommentCell? {
-        guard numberOfRenderedComments() > row else {
-            return nil
-        }
-        let dataSource = tableView.dataSource
-        let index = IndexPath(row: row, section: commentsSection)
-        return dataSource?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+        cell(row: row, section: commentsSection) as? ImageCommentCell
     }
 
     private var commentsSection: Int { 0 }
@@ -147,22 +155,26 @@ extension ListViewController {
         dataSource?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
 
+    func simulateLoadMoreFeedAction() {
+        guard let view = cell(row: 0, section: feedLoadMoreSection) else { return }
+
+        let delegate = tableView.delegate
+        let index = IndexPath(row: 0, section: feedLoadMoreSection)
+        delegate?.tableView?(tableView, willDisplay: view, forRowAt: index)
+    }
+
     func renderedFeedImageData(at index: Int) -> Data? {
         simulateFeedImageViewVisible(at: index)?.renderedImage
     }
 
     func numberOfRenderedFeedImageViews() -> Int {
-        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: feedImagesSection)
+        numberOfRows(in: feedImagesSection)
     }
 
     func feedImageView(at row: Int) -> UITableViewCell? {
-        guard numberOfRenderedFeedImageViews() > row else {
-            return nil
-        }
-        let dataSource = tableView.dataSource
-        let index = IndexPath(row: row, section: feedImagesSection)
-        return dataSource?.tableView(tableView, cellForRowAt: index)
+        cell(row: row, section: feedImagesSection)
     }
 
     private var feedImagesSection: Int { 0 }
+    private var feedLoadMoreSection: Int { 1 }
 }
--- EssentialFeed/EssentialFeediOS/Shared UI/Controllers/CellController.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/CellController.swift
@@ -23,6 +23,16 @@ public struct CellController {
         self.dataSourcePrefetching = dataSource
     }
 
+    public init(
+        id: AnyHashable,
+        _ dataSource: UITableViewDataSource & UITableViewDelegate
+    ) {
+        self.id = id
+        self.dataSource = dataSource
+        self.delegate = dataSource
+        self.dataSourcePrefetching = nil
+    }
+
     public init(id: AnyHashable, _ dataSource: UITableViewDataSource) {
         self.id = id
         self.dataSource = dataSource
