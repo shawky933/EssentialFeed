20fdd7bbc2e4e1c54d0826b6aead4bec6f53136f
Shawky Elhanak <shawky@elhanak.com>
2025-02-26 16:17:57 +0100

    Promote InMemoryFeedStore to production Replaced Dictionary with NSCache to support automatic eviction when resources are low.

--- EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -139,47 +139,6 @@ class FeedAcceptanceTests: XCTestCase {
         }
     }
 
-    private class InMemoryFeedStore: FeedStore, FeedImageDataStore {
-        private(set) var feedCache: CachedFeed?
-        private var feedImageDataCache: [URL: Data] = [:]
-
-        private init(feedCache: CachedFeed? = nil) {
-            self.feedCache = feedCache
-        }
-
-        func deleteCachedFeed() throws {
-            feedCache = nil
-        }
-
-        func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {
-            feedCache = CachedFeed(feed: feed, timestamp: timestamp)
-        }
-
-        func retrieve() throws -> CachedFeed? {
-            feedCache
-        }
-
-        func insert(_ data: Data, for url: URL) throws {
-            feedImageDataCache[url] = data
-        }
-
-        func retrieve(dataForURL url: URL) throws -> Data? {
-            feedImageDataCache[url]
-        }
-
-        static var empty: InMemoryFeedStore {
-            InMemoryFeedStore()
-        }
-
-        static var withExpiredFeedCache: InMemoryFeedStore {
-            InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
-        }
-
-        static var withNonExpiredFeedCache: InMemoryFeedStore {
-            InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
-        }
-    }
-
     private func response(for url: URL) -> (Data, HTTPURLResponse) {
         let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
         return (makeData(for: url), response)
